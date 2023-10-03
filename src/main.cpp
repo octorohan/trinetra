a85818e0c30df0c2b7fe566467e63fe725a3ac71
Rohan <aibcs2022@gmail.com>
2023-10-03 21:37:27 +0530

    first upload

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+
+const int pingTrigPin = A4;
+const int pingEchoPin = A5;
+int led = 13; // Buzzer to PIN 4
+int buz1 = 9;
+
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+    pinMode(buz1, OUTPUT);
+    pinMode(pingTrigPin, OUTPUT);
+    pinMode(pingEchoPin, INPUT);
+}
+
+void loop()
+{
+    long duration, cm;
+
+    digitalWrite(pingTrigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(pingTrigPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(pingTrigPin, LOW);
+
+    duration = pulseIn(pingEchoPin, HIGH);
+    cm = duration / 29 / 2;
+
+    if (cm <= 100 && cm > 0)
+    {
+        int d = map(cm, 1, 300, 10, 1000);
+        digitalWrite(led, HIGH);
+        digitalWrite(buz1, HIGH);
+        delay(50);
+        digitalWrite(led, LOW);
+        digitalWrite(buz1, LOW);
+        delay(d);
+    }
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+    delay(40);
+}
